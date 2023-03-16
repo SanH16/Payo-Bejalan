9312115c390f8d2bfbcf43966bf990bfbe57bb3a
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-03-16 18:39:23 +0700

    push
    TambahActivity & AdapterDestinasi

--- app/src/main/java/com/example/payobejalan/TambahActivity.java
+++ app/src/main/java/com/example/payobejalan/TambahActivity.java
@@ -3,12 +3,53 @@ package com.example.payobejalan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class TambahActivity extends AppCompatActivity {
+
+    private EditText etNama, etAlamat, etJam;
+    private Button btnTambah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah);
+
+        etNama = findViewById(R.id.et_nama);
+        etAlamat = findViewById(R.id.et_alamat);
+        etJam = findViewById(R.id.et_jam_operasi);
+        btnTambah = findViewById(R.id.btn_tambah);
+
+        btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama, alamat, jam;
+
+                nama = etNama.getText().toString();
+                alamat = etAlamat.getText().toString();
+                jam = etJam.getText().toString();
+
+                if (nama.trim().equals("")) {
+                    etNama.setError("Nama tidak boleh kosong");
+                } else if (alamat.trim().equals("")) {
+                    etAlamat.setError("Alamat tidak boleh kosong");
+                } else if (jam.trim().equals("")) {
+                    etJam.setError("Jam tidak boleh kosong");
+                }else {
+                    MyDatabaseHelper myDB = new MyDatabaseHelper(TambahActivity.this);
+                    long eks = myDB.tambahData(nama, alamat, jam);
+                    if (eks == -1) {
+                        Toast.makeText(TambahActivity.this, "Tambah Data Gagal!", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(TambahActivity.this, "Tambah Data Berhasil!", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }
+            }
+        });
     }
 }
