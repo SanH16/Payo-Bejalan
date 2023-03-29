fe693ccbb4a557cfa50864f0505d2b80e2a16669
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-03-29 17:12:33 +0700

    update

--- app/src/main/java/com/example/payobejalan/MyDatabaseHelper.java
+++ app/src/main/java/com/example/payobejalan/MyDatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.payobejalan;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -52,5 +53,17 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
         long eksekusi = db.insert(TABLE_NAME, null, cv);
         return eksekusi;
+    }
+
+    public Cursor bacaDataDestinasi(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+
+        Cursor varCursor = null;
+        if (db!=null){
+            varCursor = db.rawQuery(query, null);
+        }
+
+        return varCursor;
     }
 }
